fa148f3c0f9ed0204dafb7ddba994d1a8cf7845a
shabab477 <shababkarim93@gmail.com>
2017-08-30 14:45:14 +0600

    modify heap problem

--- src/hackerrank/HeapMedian.java
+++ src/hackerrank/HeapMedian.java
@@ -38,10 +38,10 @@ public class HeapMedian {
     public static void add(int[] array, int lastIndex, int num)
     {
         array[lastIndex] = num;
-        heapify(array, lastIndex);
     }
     
-    public static void heapify(int[] array, int lastIndex)
+    
+    public static void heapifyMax(int[] array, int lastIndex)
     {
         int parentIndex = parent(lastIndex);
         if(parentIndex >= 1)
@@ -51,29 +51,63 @@ public class HeapMedian {
                 int temp = array[lastIndex];
                 array[lastIndex] = array[parentIndex];
                 array[parentIndex] = temp;
-                heapify(array, parentIndex);
+                heapifyMax(array, parentIndex);
             }
         }
     }
     
+    public static void heapifyMin(int[] array, int lastIndex)
+    {
+        int parentIndex = parent(lastIndex);
+        if(parentIndex >= 1)
+        {
+            if(array[lastIndex] < array[parentIndex])
+            {
+                int temp = array[lastIndex];
+                array[lastIndex] = array[parentIndex];
+                array[parentIndex] = temp;
+                heapifyMin(array, parentIndex);
+            }
+        }
+    }
+    
+    
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] array = new int[n * 2 + 10];
-        int size = 0;
+        PriorityQueue<Float> minPq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Float> maxPq = new PriorityQueue<>();
         
-        for(int c= 0; c < n; c++){
-            int num = in.nextInt();
-            add(array, size + 1, num);
-            size++;
-            
-            if(size % 2 == 1)
+        for(int c = 0; c < n; c++)
+        {
+            float num = in.nextInt();
+            if(minPq.size() <= maxPq.size())
             {
-                System.out.println(array[size / 2]);
+                minPq.add(num);
             }
             else
             {
-                System.out.println((array[size / 2] + array[(size / 2) - 1]) / 2);
+                maxPq.add(num);
+            }
+            
+            if(minPq.size() != 0 && maxPq.size() != 0 && minPq.peek() > maxPq.peek())
+            {
+                float mn = minPq.remove();
+                float mx = maxPq.remove();
+                
+                maxPq.add(mn);
+                minPq.add(mx);
+            }
+            
+            if(maxPq.size() == minPq.size())
+            {
+                System.out.println(String.format("%.1f", (maxPq.peek() + minPq.peek()) / 2));
+            }
+            else
+            {
+                System.out.println(String.format("%.1f",  minPq.peek()));
+            
             }
         }
     }
